62aca719ee81f76afdad9d5538537553886b769d
Anushka-Singh-Sengar <sa22019136@gmail.com>
2026-08-18 14:41:07 +0530

    Time: 2 ms (88.88%), Space: 48.1 MB (8.76%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,19 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxReach = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if (i > maxReach) {
-                return false;
-            }
-
-            maxReach = Math.max(maxReach, i + nums[i]);
-
-            if (maxReach >= nums.length - 1) {
-                return true;
-            }
+        int f = 0;
+        for(int i =0; i <nums.length; i++){
+            if(f < i ) return false;
+            f = Math.max(f, i + nums[i]);
+            if(f>= nums.length-1){
+            return true;}
         }
 
         return true;
     }
+}
